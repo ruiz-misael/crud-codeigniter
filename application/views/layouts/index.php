226d179ade1805d9188cde7e92eb5f64722f03c0
programador <Programador@vidal.local>
2022-03-19 11:58:26 -0500

    first commit

---
+++ application/views/layouts/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" 
+    href="http://localhost/codeigniter/assets/css/bootstrap.css" />
+    <title>TASKS</title>
+    
+</head>
+<body>
+<hr>
+<br>
+<script src="http://localhost/codeigniter/assets/js/jquery.js"></script>
+<script>
+    $(document).ready(function(){
+            var n=1;
+            for(i=0;i<n;i++){
+                
+            }
+
+    });
+</script>
+</body>
+</html>
